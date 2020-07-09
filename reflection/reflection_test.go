e435eea7a8caa07167bc032cfb89bf87352df433
FarzamAlam <farzamcse@gmail.com>
2020-07-08 23:40:16 -0700

    Updated test case

--- reflection/reflection_test.go
+++ reflection/reflection_test.go
@@ -3,18 +3,21 @@ package reflection
 import "testing"
 
 func TestWalk(t *testing.T) {
-	expected := "Chris"
-	var got []string
-	x := struct {
-		Name string
-	}{expected}
-	walk(x, func(input string) {
-		got = append(got, input)
+	t.Run("Reflection Suite : ", func(t *testing.T) {
+		expected := "Chris"
+		var got []string
+		x := struct {
+			Name string
+		}{expected}
+		walk(x, func(input string) {
+			got = append(got, input)
+		})
+		if len(got) != 1 {
+			t.Errorf("Wrong number of function calls, got %d while want %d", len(got), 1)
+		}
+		if got[0] != expected {
+			t.Errorf("got %q, want %q", got[0], expected)
+		}
 	})
-	if len(got) != 1 {
-		t.Errorf("Wrong number of function calls, got %d while want %d", len(got), 1)
-	}
-	if got[0] != expected {
-		t.Errorf("got %q, want %q", got[0], expected)
-	}
+
 }
