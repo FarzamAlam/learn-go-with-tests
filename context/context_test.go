988a6420493a609f2ef31d066214ead6e98ae1f7
FarzamAlam <farzamcse@gmail.com>
2020-07-12 17:40:31 +0530

    Wrapped up context

--- context/context_test.go
+++ context/context_test.go
@@ -39,27 +39,46 @@ func (s *SpyStore) Fetch(ctx context.Context) (string, error) {
 	}
 }
 
+type SpyResponseWriter struct {
+	written bool
+}
+
+func (s *SpyResponseWriter) Header() http.Header {
+	s.written = true
+	return nil
+}
+
+func (s *SpyResponseWriter) Write([]byte) (int, error) {
+	s.written = true
+	return -1, nil
+}
+
+func (s *SpyResponseWriter) WriteHeader(statusCode int) {
+	s.written = true
+}
+
 func TestServer(t *testing.T) {
-	// t.Run("Cancel the req in 5 milli seconds :", func(t *testing.T) {
-	// 	data := "hello world"
-	// 	store := &SpyStore{response: data}
-	// 	server := Server(store)
+	t.Run("Cancel the req in 5 milli seconds :", func(t *testing.T) {
+		t.Helper()
+		data := "hello world"
+		store := &SpyStore{response: data, t: t}
+		server := Server(store)
 
-	// 	request := httptest.NewRequest(http.MethodGet, "/", nil)
-	// 	cancellingContext, cancel := context.WithCancel(request.Context())
+		request := httptest.NewRequest(http.MethodGet, "/", nil)
+		cancellingContext, cancel := context.WithCancel(request.Context())
 
-	// 	time.AfterFunc(time.Millisecond, cancel)
+		time.AfterFunc(10*time.Millisecond, cancel)
 
-	// 	request = request.WithContext(cancellingContext)
-	// 	response := httptest.NewRecorder()
+		request = request.WithContext(cancellingContext)
+		response := &SpyResponseWriter{}
 
-	// 	server.ServeHTTP(response, request)
+		server.ServeHTTP(response, request)
 
-	// 	if response.Body.String() == data {
-	// 		t.Errorf("got %s while want %s", response.Body.String(), data)
-	// 	}
+		if response.written {
+			t.Errorf("a response has not been written.")
+		}
 
-	// })
+	})
 	t.Run("match the fetch method : ", func(t *testing.T) {
 		data := "hello world"
 		store := &SpyStore{response: data, t: t}
