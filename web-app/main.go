5725db338207b6912571d4dfc6876299c7009506
FarzamAlam <farzamcse@gmail.com>
2020-07-26 23:35:50 +0530

    Added test for response code

--- web-app/main.go
+++ web-app/main.go
@@ -5,8 +5,13 @@ import (
 	"net/http"
 )
 
+type InMemoryPlayerStore struct{}
+
+func (i *InMemoryPlayerStore) GetPlayerScore(name string) int {
+	return 123
+}
 func main() {
-	server := &PlayerServer{}
+	server := &PlayerServer{&InMemoryPlayerStore{}}
 	port := "5000"
 	log.Println("Starting server at :", port)
 	http.ListenAndServe(":"+port, server)
--- web-app/server_test.go
+++ web-app/server_test.go
@@ -21,6 +21,7 @@ func TestGETPlayers(t *testing.T) {
 
 		got := response.Body.String()
 		want := "10"
+		assertResponseCode(t, response.Code, http.StatusOK)
 		assertResponse(t, got, want)
 	})
 	t.Run("Pepper Request : ", func(t *testing.T) {
@@ -30,9 +31,25 @@ func TestGETPlayers(t *testing.T) {
 
 		got := response.Body.String()
 		want := "20"
+		assertResponseCode(t, response.Code, http.StatusOK)
 		assertResponse(t, got, want)
 	})
+	t.Run("Apollo 404 Request : ", func(t *testing.T) {
+		request := newGetScoreRequest("Apollo")
+		response := httptest.NewRecorder()
+		server.ServeHTTP(response, request)
 
+		got := response.Code
+		want := http.StatusNotFound
+		assertResponseCode(t, got, want)
+	})
+}
+
+func assertResponseCode(t *testing.T, got, want int) {
+	t.Helper()
+	if got != want {
+		t.Errorf("got %d while want %d", got, want)
+	}
 }
 
 func assertResponse(t *testing.T, got, want string) {
